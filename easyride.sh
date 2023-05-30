29ea75c3a6180fbf3126b513a3602321a3a34f3a
P-B Monaco <imfonky@gmail.com>
2023-05-30 22:03:35 +0200

    Forgot to end string

--- easyride.sh
+++ easyride.sh
@@ -2,5 +2,5 @@
 
 ./profile.sh sw24978_15.tsp results theplot
 
-echo "Results can be seen at path './data/results/'
-echo "Plot can be seen at path './plots/theplot.jpeg'
+echo "Results can be seen at path './data/results/'"
+echo "Plot can be seen at path './plots/theplot.jpeg'"
